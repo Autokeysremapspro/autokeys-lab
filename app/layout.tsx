b8d91e426fd245f0a50f8cb1b315b2b6a8b884e8
Autokeysremapspro <info@autokeyspro.es>
2026-08-23 01:23:37 +0200

    feat: wire supplied ERP login artwork into app shell

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import './visual-polish.css'
+import './login-art.css'
 import type { Metadata } from 'next'
 import { Space_Grotesk, JetBrains_Mono, Inter } from 'next/font/google'
 import { Toaster } from 'react-hot-toast'
